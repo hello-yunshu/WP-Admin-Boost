bae1f3a16f8fe0de4aea77c31a40b34bdab93e74
潘羿 <33997505+paniy@users.noreply.github.com>
2021-02-04 19:29:58 +0800

    Update wp-admin-boost.php

--- wp-admin-boost.php
+++ wp-admin-boost.php
@@ -4,7 +4,7 @@
  * Description: 使用jsdelivr加速WordPress的后台核心小文件与插件小文件，大幅提高后台访问速度。
  * Author: 潘羿
  * Author URI:https://www.idleleo.com/
- * Version: 1.0.2
+ * Version: 1.0.3
  * Network: True
  * License: GPLv3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -54,7 +54,7 @@ class WP_ADMIN_BOOST
             if (get_option('wpab_admin') != 2 && !stristr($GLOBALS['wp_version'], 'alpha') && !stristr($GLOBALS['wp_version'], 'beta')) {
                 add_action('init', function () {
                     ob_start(function ($buffer) {
-                        $buffer = preg_replace('~' . home_url('/') . '(wp-admin|wp-includes)/(css|js)/~', sprintf('//cdn.jsdelivr.net/gh/WordPress/WordPress@%s/$1/$2/', $GLOBALS['wp_version']), $buffer);
+                        $buffer = preg_replace('~' . home_url('/') . '(wp-admin|wp-includes)/(css|js)/(?:.*).(js|css|jpg|png|gif|svg|webp)~', sprintf('//cdn.jsdelivr.net/gh/WordPress/WordPress@%s/$1/$2/', $GLOBALS['wp_version']), $buffer);
                         return $buffer;
                     });
                 });
